fdb675387f99f77c6ddd2d9cc4b62867ace7d369
Ismael Medeiros <140083162@aluno.unb.br>
2017-12-05 15:15:09 -0200

    Removed use of license path on operator;

--- src/PkiExpressOperator.php
+++ src/PkiExpressOperator.php
@@ -51,10 +51,6 @@ abstract class PkiExpressOperator
 
     protected function invoke($command, array $args = array())
     {
-        if (empty($this->config->getLicensePath())) {
-            throw new \Exception("The license's path was not set");
-        }
-
         // Add PKI Express invocation arguments
         $cmdArgs = array();
         foreach ($this->getPkiExpressInvocation() as $invocationArg) {
@@ -66,10 +62,6 @@ abstract class PkiExpressOperator
 
         // Add PKI Express arguments
         $cmdArgs = array_merge($cmdArgs, $args);
-
-        // Add the license path
-        $cmdArgs[] = "-lf";
-        $cmdArgs[] = $this->config->getLicensePath();
 
         // Add file references if added
         if (!empty($this->fileReferences)) {
