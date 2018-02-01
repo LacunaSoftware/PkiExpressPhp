abbb1c067ead0259f3689edf74a29220eb9e59fb
Ismael Medeiros <140083162@aluno.unb.br>
2018-02-01 15:44:46 -0200

    Added new class;

---
+++ src/ResourceContentOrReference.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lacuna\PkiExpress;
+
+
+class ResourceContentOrReference
+{
+    public $url;
+    public $mimeType;
+    public $content;
+
+    public function __construct()
+    {
+    }
+}
