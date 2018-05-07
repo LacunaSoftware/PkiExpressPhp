ced4682ac1e76ff12d0d31de7277c8e285c2c4d9
Ismael Medeiros <IsmaelM@lacunasoftware.com>
2018-05-07 15:02:43 -0300

    Changes following the new policy names.

--- src/StandardSignaturePolicies.php
+++ src/StandardSignaturePolicies.php
@@ -6,10 +6,10 @@ namespace Lacuna\PkiExpress;
 class StandardSignaturePolicies
 {
     // CAdES policies
-    const PKI_BRAZIL_CADES_ADR_BASICA = 'ad-rb';
-    const PKI_BRAZIL_CADES_ADR_BASICA_WITH_REVOCATION_VALUES = 'ad-rb-rv';
-    const PKI_BRAZIL_CADES_ADR_TEMPO = 'ad-rt';
-    const PKI_BRAZIL_CADES_ADR_COMPLETA = 'ad-rc';
+    const PKI_BRAZIL_CADES_ADR_BASICA = 'adrb';
+    const PKI_BRAZIL_CADES_ADR_BASICA_WITH_REVOCATION_VALUES = 'adrb-rv';
+    const PKI_BRAZIL_CADES_ADR_TEMPO = 'adrt';
+    const PKI_BRAZIL_CADES_ADR_COMPLETA = 'adrc';
     const CADES_BES = 'cades';
     const CADES_BES_WITH_REVOCATION_VALUES = 'cades-rv';
     const CADES_T = 'cades-t';
@@ -22,8 +22,8 @@ class StandardSignaturePolicies
     const NFE_PADRAO_NACIONAL = 'nfe';
     const XADES_BES = 'xades';
     const XML_DSIG_BASIC = 'basic';
-    const PKI_BRAZIL_XML_ADR_BASICA = 'ad-rb';
-    const PKI_BRAZIL_XML_ADR_TEMPO = 'ad-rt';
+    const PKI_BRAZIL_XML_ADR_BASICA = 'adrb';
+    const PKI_BRAZIL_XML_ADR_TEMPO = 'adrt';
 
     public static function requireTimestamp($policy)
     {
